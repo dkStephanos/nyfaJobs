2ccba0eedede33e8c907e2828c0785c9c5add913
dkStephanos <stephanos@goldmail.etsu.edu>
2018-05-07 20:13:51 -0400

    Set up about page, need to convert to actual job titles

--- NYFAJobs/Controllers/HomeController.cs
+++ NYFAJobs/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NYFAJobs.DAL;
+using NYFAJobs.ViewModels;
 
 namespace NYFAJobs.Controllers
 {
     public class HomeController : Controller
     {
+        private BoardContext db = new BoardContext();
+
         public ActionResult Index()
         {
             return View();
@@ -15,9 +19,14 @@ namespace NYFAJobs.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            IQueryable<ApplicationGroup> data = from application in db.Applications
+                                                   group application by application.CandidateID into applicationGroup
+                                                   select new ApplicationGroup()
+                                                   {
+                                                       JobTitle = applicationGroup.Key.ToString(),
+                                                       ApplicationCount = applicationGroup.Count()
+                                                   };
+            return View(data.ToList());
         }
 
         public ActionResult Contact()
@@ -25,6 +34,12 @@ namespace NYFAJobs.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
---
+++ NYFAJobs/ViewModels/ApplicationGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NYFAJobs.ViewModels
+{
+    public class ApplicationGroup
+    {
+        [DataType(DataType.Text)]
+        public string JobTitle { get; set; }
+
+        public int ApplicationCount { get; set; }
+    }
+}
