488dafe7e8af77a6cd1dec46d00bf6a1268fe9ae
dkStephanos <stephanos@goldmail.etsu.edu>
2018-05-09 20:48:35 -0400

    Added office assignment model

---
+++ NYFAJobs/Models/OfficeAssignment.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NYFAJobs.Models
+{
+    public class OfficeAssignment
+    {
+        [Key]
+        [ForeignKey("Interviewer")]
+        public int InterviewerID { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Office Location")]
+        public string Location { get; set; }
+
+        public virtual Interviewer Interviewer { get; set; }
+    }
+}
