0c7d15adcd5a919b753ba788b43a3c1e20307b63
dkStephanos <stephanos@goldmail.etsu.edu>
2018-05-09 21:16:25 -0400

    Created Company model

---
+++ NYFAJobs/Models/Company.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NYFAJobs.Models
+{
+    public class Company
+    {
+        public int CompanyID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Industry { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        public decimal Budget { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+        public int? InterviewerID { get; set; }
+
+        public virtual Interviewer Administrator { get; set; }
+        public virtual ICollection<Job> Jobs { get; set; }
+    }
+}
