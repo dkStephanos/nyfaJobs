3d483281b7c9c7043d994800f371cfc69954978a
dkStephanos <stephanos@goldmail.etsu.edu>
2018-05-09 20:53:40 -0400

    Added validations and collectio properties to Job

--- NYFAJobs/Models/Job.cs
+++ NYFAJobs/Models/Job.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -9,11 +11,22 @@ namespace NYFAJobs.Models
     public class Job
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
         public int JobID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
         public string PositionType { get; set; }
+
+        [Range(0, 1000000)]
         public double Salary { get; set; }
 
+        public int CompanyID { get; set; }
+
         public virtual ICollection<Application> Applications { get; set; }
+        public virtual Company Companies { get; set; }
+        public virtual ICollection<Interviewer> Interviewers { get; set; }
     }
 }
