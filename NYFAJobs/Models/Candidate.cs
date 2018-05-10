2dda9c5e9f37cfbd354415acf0b257d0e52a4427
dkStephanos <stephanos@goldmail.etsu.edu>
2018-05-09 20:46:16 -0400

    Added validations for Candidate properties, added full name method

--- NYFAJobs/Models/Candidate.cs
+++ NYFAJobs/Models/Candidate.cs
@@ -9,13 +9,26 @@ namespace NYFAJobs.Models
     public class Candidate
     {
         public int ID { get; set; }
+        [Required]
         [StringLength(50)]
+        [Display(Name = "Last Name")]
         [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         public string LastName { get; set; }
+        [Display(Name = "First Name")]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         public string FirstName { get; set; }
+        [Required]
         public string Degree { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
 
         public virtual ICollection<Application> Applications { get; set; }
     }
